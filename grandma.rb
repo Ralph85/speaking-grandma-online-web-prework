0b17c91d0ef5cb7879900a469387d3e931de8b12
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2018-12-27 05:38:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,7 @@ elsif "HUH? SPEAK UP, SONNY!"
 elsif "I LOVE YOU GRANDMA!"
 
 elsif "NOT SINCE 1938!"
-# and yells back
 
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+elsif "I LOVE YOU GRANDMA!"
+else "I LOVE YOU TOO HONEY!"
+END
